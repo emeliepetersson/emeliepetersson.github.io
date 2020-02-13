d3a9d99960d723420d0528c683a25cc3247d998a
emeliepetersson <emepet1204@skola.goteborg.se>
2020-02-13 11:26:30 +0100

    change layout and content

---
+++ script.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const iconsWrapper = document.querySelector(".icons");
+const icons = document.querySelectorAll(".icon");
+const back = document.querySelector(".back");
+const iconsHeading = document.querySelector("#icons-heading");
+const about = document.querySelector("#about");
+const education = document.querySelector("#education");
+const contact = document.querySelector("#contact");
+const contents = document.querySelectorAll(".content");
+
+const showMore = event => {
+  const target = event.currentTarget;
+
+  if (target.classList.contains("avatar")) {
+    about.classList.add("show");
+  } else if (target.classList.contains("education")) {
+    education.classList.add("show");
+  } else if (target.classList.contains("contact")) {
+    contact.classList.add("show");
+  }
+
+  iconsHeading.classList.add("hide");
+  back.classList.add("show");
+  iconsWrapper.classList.add("hide");
+};
+
+const goBack = event => {
+  contents.forEach(content => {
+    content.classList.remove("show");
+  });
+
+  event.currentTarget.classList.remove("show");
+  iconsWrapper.classList.remove("hide");
+  iconsHeading.classList.remove("hide");
+};
+
+icons.forEach(icon => {
+  icon.addEventListener("click", showMore);
+});
+
+back.addEventListener("click", goBack);
